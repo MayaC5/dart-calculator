a2687135e23e2d5e4cdd09bbbda8f6c8e05b66dd
Maya Chan <mayachanym@gmail.com>
2026-03-28 14:20:14 +0000

    update game01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex">
+        <div className="flex flex-col gap-4">
           <Link href="/games/game01">
             <div>01 Games</div>
           </Link>
