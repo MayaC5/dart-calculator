8cebf27e3e1eddc8597ed9eeeca4317136c4260e
Maya Chan <mayachanym@gmail.com>
2026-03-31 08:52:05 +0100

    hide unuse page

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,21 +1,21 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 
-export default function Setting() {
+// export default function Setting() {
 
-    const setBoardMode = (mode: string) => {
-        console.log("set board mode to ", mode)
-        localStorage.setItem("boardMode", mode)
-    }
+//     const setBoardMode = (mode: string) => {
+//         console.log("set board mode to ", mode)
+//         localStorage.setItem("boardMode", mode)
+//     }
 
 
-    return (
-      <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        board mode setting
-        <Button onClick={() => setBoardMode("Input")}>Input</Button>
-        <Button onClick={() => setBoardMode("Press Button")}>Press Button</Button>
-        <Button onClick={() => setBoardMode("Board Button")}>Board Button</Button>
-        <Button onClick={() => setBoardMode("Press Value")}>Press Value</Button>
-</div>
+//     return (
+//       <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+//         board mode setting
+//         <Button onClick={() => setBoardMode("Input")}>Input</Button>
+//         <Button onClick={() => setBoardMode("Press Button")}>Press Button</Button>
+//         <Button onClick={() => setBoardMode("Board Button")}>Board Button</Button>
+//         <Button onClick={() => setBoardMode("Press Value")}>Press Value</Button>
+// </div>
 
-    );
-}
+//     );
+// }
